5d2b9044061149f0bcb5fdccbb45f752962eed0e
4ngel <bootyeat913@gmail.com>
2025-01-12 03:56:09 -0500

    Update title2.js

--- Assets/js/title2.js
+++ Assets/js/title2.js
@@ -7,7 +7,7 @@ document.addEventListener ? document.addEventListener("DOMContentLoaded", (funct
     loaded()
 }));
 var x = 0,
-    titleText = ["fuck skids", "im hot", "fuck faggots","doxbin.com", "doxbin.org","kkk member A-ZA-Z0-9","#wehateskids","doxbin.net","don't skid :)"];
+    titleText = ["Twin Starzz", "333"];
 
 function loop() {
     document.getElementsByTagName("title")[0].innerHTML = titleText[x++ % titleText.length]
